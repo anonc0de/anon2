02294b53743e83928582460d8a1cf4b6322e92f4
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-09-29 20:17:38 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -33,7 +33,9 @@ OPENAI_KEY = os.getenv(
 
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://ilxnvuj833:0212@cluster0.ile0pdo.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://dinexa1064:0212@cluster0.ep5evwf.mongodb.net/?retryWrites=true&w=majority",
 )
 
 #mongodb+srv://dinexa1064:0212@cluster0.ep5evwf.mongodb.net/?retryWrites=true&w=majority
+
+#mongodb+srv://ilxnvuj833:0212@cluster0.ile0pdo.mongodb.net/?retryWrites=true&w=majority
