80d123fefec15433cbbcecfe7bf62f00ad0afbdf
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-09-30 08:47:51 +0700

    Update kng.py

--- PyroUbot/core/plugins/kng.py
+++ PyroUbot/core/plugins/kng.py
@@ -90,7 +90,7 @@ async def kang(client, message):
 
         u_name = user.username
         u_name = "@" + u_name if u_name else user.first_name or user.id
-        packname = f"ꜱᴛɪᴄᴋᴇʀꜱ ᴘᴀᴄᴋ {user.id} _V{pack}"
+        packname = f"ꜱᴛɪᴄᴋᴇʀꜱ_ᴘᴀᴄᴋ{user.id}_V{pack}"
         custom_packnick = f"{u_name} ᴘᴀᴄᴋ"
         packnick = f"{custom_packnick} Vol.{pack}"
         cmd = "/newpack"
@@ -145,7 +145,7 @@ async def kang(client, message):
             limit = "50" if is_anim else "120"
             while limit in await get_response(message, client):
                 pack += 1
-                packname = f"a{user.id}_by_{user.username}_{pack}"
+                packname = f"_{user.id}_by_{user.username}_{pack}"
                 packnick = f"{custom_packnick} vol.{pack}"
                 if is_anim:
                     packname += "_anim"
