3047a2f15f8b5fcace2ff9cddfc6bc073416e292
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-09-30 22:59:30 +0700

    Update plugins.py

--- PyroUbot/core/function/plugins.py
+++ PyroUbot/core/function/plugins.py
@@ -16,12 +16,10 @@ async def loadPlugins():
     modules = loadModule()
     for mod in modules:
         imported_module = import_module(f"PyroUbot.modules.{mod}")
-        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
-            if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
-                HELP_COMMANDS[
-                    imported_module.__MODULE__.replace(" ", "_").lower()
-                ] = imported_module
+        module_name = imported_module.__MODULE__
+        if module_name:
+            module_name = module_name.replace(" ", "_").lower()
+            HELP_COMMANDS[module_name] = imported_module
     print(f"[🤖 @{bot.me.username} 🤖] [🔥 TELAH BERHASIL DIAKTIFKAN! 🔥]")
     TM = await bot.send_message(
         OWNER_ID,
