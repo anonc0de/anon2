6cdada7d776be09ed843ef80b6d8acd2fc351ceb
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-02 09:04:28 +0700

    Update __init__.py

--- PyroUbot/modules/__init__.py
+++ PyroUbot/modules/__init__.py
@@ -1,7 +1,7 @@
 from glob import glob
 from os.path import basename, dirname, isfile
 
-
+"""
 def loadModule():
     mod_paths = glob(f"{dirname(__file__)}/*.py")
     return sorted(
@@ -11,3 +11,11 @@ def loadModule():
             if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
         ]
     )
+"""
+
+def loadModule():
+    return sorted([
+        basename(f)[:-3]
+        for f in glob(f"{dirname(__file__)}/*.py")
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ])
