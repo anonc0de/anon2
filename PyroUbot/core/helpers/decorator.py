a7e77a6779145eec7a5ee2c301d410bfeda673c0
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-03 17:18:17 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -5,15 +5,34 @@ from pyrogram.enums import ChatType
 from PyroUbot import OWNER_ID, bot, ubot
 
 
-async def install_my_peer(client):
-    users = []
-    groups = []
+async def get_global_id(client, query):
+    chats = []
+    chat_types = {
+        "global": [ChatType.CHANNEL, ChatType.GROUP, ChatType.SUPERGROUP],
+        "group": [ChatType.GROUP, ChatType.SUPERGROUP],
+        "users": [ChatType.PRIVATE],
+    }
     async for dialog in client.get_dialogs(limit=None):
-        if dialog.chat.type == ChatType.PRIVATE:
-            users.append(dialog.chat.id)
-        elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-            groups.append(dialog.chat.id)
-    client._get_my_peer[client.me.id] = {"pm": users, "gc": groups}
+        if dialog.chat.type in chat_types[query]:
+            chats.append(dialog.chat.id)
+
+    return chats
+
+
+async def install_my_peer(client):
+    users = [
+        dialog.chat.id
+        async for dialog in client.get_dialogs(limit=None)
+        if dialog.chat.type == ChatType.PRIVATE
+    ]
+    groups = [
+        dialog.chat.id
+        async for dialog in client.get_dialogs(limit=None)
+        if dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP)
+    ]
+    client_id = client.me.id
+
+    client._get_my_peer[client_id] = {"pm": users, "gc": groups}
 
 
 async def installPeer():
