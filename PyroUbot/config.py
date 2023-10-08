56264de6bf3d6ffd44b427f09b70b1ee2311d5f5
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-08 19:10:23 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -2,7 +2,7 @@ import os
 
 DEVS = [
     1054295664,
-    1933872185,
+    6629259024,
 ]
 
 API_ID = int(os.getenv("API_ID", "24623085"))
@@ -17,7 +17,7 @@ USER_ID = list(map(int,os.getenv("USER_ID", "6629259024 722852368 6055603026 132
 
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001538826310"))
 
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001538826310 -1001462256506 -1001812143750 -1001936454178").split()))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001538826310").split()))
 
 MAX_BOT = int(os.getenv("MAX_BOT", "550"))
 
