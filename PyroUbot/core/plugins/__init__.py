9853578834aff845ef28acccdd6a4df2d4bbc4b5
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-08 14:52:49 +0700

    Update __init__.py

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -42,3 +42,4 @@ from PyroUbot.core.plugins.whs import *
 from PyroUbot.core.plugins.ytp import *
 from PyroUbot.core.plugins.zbs import *
 from PyroUbot.core.plugins.tr import *
+from PyroUbot.core.plugins.gratis import *
