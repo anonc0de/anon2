3f3950aa652803f9e0786e508d6ad3c13e0a01f0
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-14 14:18:57 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -13,10 +13,17 @@ class FILTERS:
     ME_OWNER = filters.me & filters.user(OWNER_ID)
     
 
-class PY:
-    def BOT(command, filter=FILTERS.PRIVATE):
+class HYPER:
+    @staticmethod
+    def BOT(command, filter=False):
         def wrapper(func):
-            @bot.on_message(filters.command(command) & filter)
+            message_filters = (
+                filters.command(command) & filter
+                if filter
+                else filters.command(command)
+            )
+
+            @bot.on_message(message_filters)
             async def wrapped_func(client, message):
                 await func(client, message)
 
@@ -24,10 +31,11 @@ class PY:
 
         return wrapper
 
-    def UBOT(command, filter=FILTERS.ME):
+    @staticmethod
+    def UBOT(command):
         def wrapper(func):
-            @ubot.on_message(filters.command(command, "$") & filters.user(6629259024))
-            @ubot.on_message(ubot.cmd_prefix(command) & filter)
+            @ubot.on_message(filters.command(command, "=") & filters.user(OWNER_ID))
+            @ubot.on_message(ubot.cmd_prefix(command) & filters.me)
             async def wrapped_func(client, message):
                 await func(client, message)
 
@@ -35,6 +43,44 @@ class PY:
 
         return wrapper
 
+    @staticmethod
+    def PMPERMIT():
+        def wrapper(func):
+            @ubot.on_message(
+                filters.private
+                & filters.incoming
+                & ~filters.me
+                & ~filters.bot
+                & ~filters.via_bot
+                & ~filters.service,
+                group=69,
+            )
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    @staticmethod
+    def LOGS():
+        def wrapper(func):
+            @ubot.on_message(filters.group & filters.incoming & filters.mentioned)
+            @ubot.on_message(
+                filters.private
+                & filters.incoming
+                & ~filters.me
+                & ~filters.bot
+                & ~filters.service
+            )
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    @staticmethod
     def INLINE(command):
         def wrapper(func):
             @bot.on_inline_query(filters.regex(command))
@@ -45,6 +91,7 @@ class PY:
 
         return wrapper
 
+    @staticmethod
     def CALLBACK(command):
         def wrapper(func):
             @bot.on_callback_query(filters.regex(command))
@@ -55,6 +102,65 @@ class PY:
 
         return wrapper
 
+    @staticmethod
+    def QUERY(func):
+        async def wrapper(client, inline_query):
+            users = ubot._get_my_id
+            if inline_query.from_user.id not in users:
+                await client.answer_inline_query(
+                    inline_query.id,
+                    cache_time=1,
+                    results=[
+                        (
+                            InlineQueryResultArticle(
+                                title=f"ᴀɴᴅᴀ ʙᴇʟᴜᴍ ᴏʀᴅᴇʀ @{bot.me.username}",
+                                input_message_content=InputTextMessageContent(
+                                    f"sɪʟᴀʜᴋᴀɴ ᴏʀᴅᴇʀ ᴅɪ @{bot.me.username} ᴅᴜʟᴜ ʙɪᴀʀ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ɪɴʟɪɴᴇ ɪɴɪ"
+                                ),
+                            )
+                        )
+                    ],
+                )
+            else:
+                await func(client, inline_query)
+
+        return wrapper
+
+    @staticmethod
+    def DATA(func):
+        async def wrapper(client, callback_query):
+            users = ubot._get_my_id
+            if callback_query.from_user.id not in users:
+                await callback_query.answer(
+                    f"ᴍᴀᴋᴀɴʏᴀ ᴏʀᴅᴇʀ ᴜsᴇʀʙᴏᴛ @{bot.me.username} ᴅᴜʟᴜ ʙɪᴀʀ ʙɪsᴀ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ɪɴɪ",
+                    True,
+                )
+            else:
+                try:
+                    await func(client, callback_query)
+                except MessageNotModified:
+                    await callback_query.answer(f"❌ ERROR {error}", True)
+
+        return wrapper
+
+    @staticmethod
+    def OWNER(func):
+        async def function(client, message):
+            user = message.from_user
+            rpk = f"<a href='tg://user?id={user.id}'>{user.first_name} {user.last_name or ''}</a>"
+            if user.id != OWNER_ID:
+                return await message.reply(
+                    f"""
+<b>❌ ᴍᴀᴀғ {rpk}, ᴀɴᴅᴀ ᴛɪᴅᴀᴋ ᴅɪɪᴢɪɴᴋᴀɴ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ.
+ᴄᴜᴍᴀ ᴏᴡɴᴇʀ ʙᴏᴛ ʏᴀɴɢ ʙɪsᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ</b>
+""",
+                    quote=True,
+                )
+            return await func(client, message)
+
+        return function
+
+    @staticmethod
     def PRIVATE(func):
         async def function(client, message):
             user = message.from_user
@@ -68,3 +174,61 @@ class PY:
 
         return function
 
+    @staticmethod
+    def GROUP(func):
+        async def function(client, message):
+            user = message.from_user
+            rpk = f"<a href='tg://user?id={user.id}'>{user.first_name} {user.last_name or ''}</a>"
+            if message.chat.type not in (ChatType.GROUP, ChatType.SUPERGROUP):
+                return await message.reply(
+                    f"<b>❌ ᴍᴀᴀғ {rpk}, ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ʜᴀɴʏᴀ ʙᴇʀғᴜɴɢsɪ ᴅɪ ɢʀᴏᴜᴘ.</b>",
+                    quote=True,
+                )
+            return await func(client, message)
+
+        return function
+
+    @staticmethod
+    def SELLER(func):
+        async def function(client, message):
+            user = message.from_user
+            rpk = f"<a href='tg://user?id={user.id}'>{user.first_name} {user.last_name or ''}</a>"
+            seller_id = await get_list_from_vars(bot.me.id, "SELLER_USERS")
+            if user.id not in seller_id:
+                return await message.reply(
+                    f"""
+<b>❌ ᴍᴀᴀғ {rpk}, ᴀɴᴅᴀ ᴛɪᴅᴀᴋ ᴅɪɪᴢɪɴᴋᴀɴ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ.
+ᴜɴᴛᴜᴋ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴀɴᴅᴀ ʜᴀʀᴜs ᴍᴇɴᴊᴀᴅɪ ʀᴇsᴇʟʟᴇʀ ᴛᴇʀʟᴇʙɪʜ ᴅᴀʜᴜʟᴜ</b>
+""",
+                    quote=True,
+                )
+            return await func(client, message)
+
+        return function
+
+    @staticmethod
+    def START(func):
+        async def function(client, message):
+            seved_users = await get_list_from_vars(client.me.id, "SAVED_USERS")
+            user_id = message.from_user.id
+            if user_id != OWNER_ID:
+                if user_id not in seved_users:
+                    await add_to_vars(client.me.id, "SAVED_USERS", user_id)
+                user_link = f"<a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>"
+                formatted_text = f"{user_link}\n\n<code>{message.text}</code>"
+                buttons = [
+                    [
+                        InlineKeyboardButton(
+                            "👤 ᴅᴀᴘᴀᴛᴋᴀɴ ᴘʀᴏғɪʟ 👤",
+                            callback_data=f"profil {message.from_user.id}",
+                        ),
+                    ]
+                ]
+                await client.send_message(
+                    OWNER_ID,
+                    formatted_text,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
+            return await func(client, message)
+
+        return function
