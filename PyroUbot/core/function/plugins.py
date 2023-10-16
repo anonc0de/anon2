7ce736af7afb89a651233748e0a0a032e7d0058d
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-16 11:44:43 +0700

    Update plugins.py

--- PyroUbot/core/function/plugins.py
+++ PyroUbot/core/function/plugins.py
@@ -1,14 +1,8 @@
 from importlib import import_module
-from platform import python_version
 
-from pytz import timezone
-from datetime import datetime
-
-from pyrogram import __version__
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from PyroUbot import bot, ubot
-from PyroUbot.config import LOGS_MAKER_UBOT
 from PyroUbot.core.helpers import PY
 from PyroUbot.modules import loadModule
 
@@ -16,9 +10,6 @@ HELP_COMMANDS = {}
 
 
 async def loadPlugins():
-    now = datetime.now(timezone("Asia/Jakarta"))
-    time = now.strftime("%d-%m-%Y")
-    clock = now.strftime("%H:%M:%S")
     modules = loadModule()
     for mod in modules:
         imported_module = import_module(f"PyroUbot.modules.{mod}")
@@ -26,20 +17,7 @@ async def loadPlugins():
         if module_name:
             HELP_COMMANDS[module_name] = imported_module
     print(f"[🤖 @{bot.me.username} 🤖] [🔥 TELAH BERHASIL DIAKTIFKAN! 🔥]")
-    TM = await bot.send_message(
-        LOGS_MAKER_UBOT,
-        f"""
-<b>🤖 ᴜsᴇʀʙᴏᴛ ʙᴇʀʜᴀsɪʟ ᴅɪᴀᴋᴛɪғᴋᴀɴ</b>
-<b>📕 ᴘʏᴛʜᴏɴ: {python_version()}</b>
-<b>📙 ᴘʏʀᴏɢʀᴀᴍ: {__version__}</b>
-<b>👤 ᴜsᴇʀ: {len(ubot._ubot)}</b>
-<b>📆 ᴅᴀᴛᴇ: {time}</b>
-<b>⏰ ᴛɪᴍᴇ: {clock}</b>
-""",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("CLOSED", callback_data="0_cls")]],
-        ),
-    )
+    
     
     
 
