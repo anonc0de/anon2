d9c5b94a5ef2a4454cd3fa37299629d94e1f494c
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 00:35:38 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -7,14 +7,13 @@ from PyroUbot import *
 async def get_top_module(client, message):
     text = "<b>🗂️ᴅᴀғᴛᴀʀ ᴍᴏᴅᴜʟᴇ ᴜʙᴏᴛ\n</b>"
     modules = loadModule()
-    count = top_command()
     for mod in modules:
         try:
             imported_module = import_module(f"PyroUbot.modules.{mod}")
             module_name = getattr(imported_module, "__MODULE__", "").replace(" ", "_").lower()
         except Exception:
             continue
-        text += f"•> {module_name} : {count} \n"
+        text += f"•> {module_name} : \n"
     if not text:
         await message.reply_text("ᴛɪᴅᴀᴋ")
     else:
