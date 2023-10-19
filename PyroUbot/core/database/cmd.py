105494ae56ce5befd3bdcf6264886c277cf191db
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 04:17:15 +0700

    Update cmd.py

--- PyroUbot/core/database/cmd.py
+++ PyroUbot/core/database/cmd.py
@@ -13,9 +13,10 @@ def update_module(module_name):
         module_usage_collection.insert_one({"module_name": module_name, "usage_count": 1})
 
 
-def module_usage(module_name):
-    module_doc = module_usage_collection.find_one({"module_name": module_name})
-    if module_doc:
-        return module_doc["usage_count"]
-    else:
-        return 0
+def usage_module():
+    result_list = []
+    for module_doc in module_usage_collection.find():
+        module_name = module_doc["module_name"]
+        usage_count = module_doc["usage_count"]
+        result_list.append({"module_name": module_name, "usage_count": usage_count})
+    return result_list
