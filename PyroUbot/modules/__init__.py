74e6f1b2b2993cf2c982b2361772790dcf3f16d0
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 00:12:48 +0700

    Update __init__.py

--- PyroUbot/modules/__init__.py
+++ PyroUbot/modules/__init__.py
@@ -1,10 +1,37 @@
+from os.path import basename, dirname
 from glob import glob
-from os.path import basename, dirname, isfile
+from os.path import isfile
 
-
+"""
 def loadModule():
     return sorted([
         basename(f)[:-3]
         for f in glob(f"{dirname(__file__)}/*.py")
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ])
+"""
+
+
+def loadModule():
+    module_list = sorted([
+        basename(f)[:-3]
+        for f in glob(f"{dirname(__file__)}/*.py")
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ])    
+    
+    module_counts = {}
+    for module in module_list:
+        if module in module_counts:
+            module_counts[module] += 1
+        else:
+            module_counts[module] = 1
+
+    return module_list
+
+    
+    # Menampilkan jumlah setiap modul yang digunakan
+    """
+    for module, count in module_counts.items():
+        print(f"Modul {module} digunakan sebanyak {count} kali.")
+    """
+    
