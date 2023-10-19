5ef2b93953ea6ecbac4c606048e46be0f4ab311b
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 01:58:39 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -1,14 +1,10 @@
+from importlib import import_module
+
 from PyroUbot import *
 
+
+
 async def get_top_module(client, message):
-    text = "<b>🗂️ᴅᴀғᴛᴀʀ ᴍᴏᴅᴜʟᴇ ᴜʙᴏᴛ\n</b>"
-    if get_arg(message) in HELP_COMMANDS:
-        await bot.send_message(
-                HELP_COMMANDS.__MODULE__ + "\n<b> ohmyshinch.t.me </b>",
-                quote=True,
-            )
-        
-async def get_prem_user(client, message):
     text = "<b>🗂️ᴅᴀғᴛᴀʀ ᴍᴏᴅᴜʟᴇ ᴜʙᴏᴛ\n</b>"
     modules = loadModule()
     for mod in modules:
