9da352ca18f804dd184519703f4399cc4ca18268
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 04:37:36 +0700

    Update cmd.py

--- PyroUbot/core/database/cmd.py
+++ PyroUbot/core/database/cmd.py
@@ -3,20 +3,22 @@ from PyroUbot.core.database import mongodb
 module_usage_collection = mongodb.module
 
 def update_module(module_name):
-    module_doc = module_usage_collection.find_one({"module_name": module_name})
-    if module_doc:
-        module_usage_collection.update_one(
-            {"module_name": module_name},
-            {"$inc": {"usage_count": 1}}
-        )
-    else:
-        module_usage_collection.insert_one({"module_name": module_name, "usage_count": 1})
+    try:
+        module_doc = module_usage_collection.find_one({"module_name": module_name})
+        if module_doc:
+            module_usage_collection.update_one(
+                {"module_name": module_name},
+                {"$inc": {"usage_count": 1}}
+            )
+        else:
+            module_usage_collection.insert_one({"module_name": module_name, "usage_count": 1})
 
 
 def usage_module():
     result_list = []
-    for module_doc in module_usage_collection.find():
-        module_name = module_doc["module_name"]
-        usage_count = module_doc["usage_count"]
-        result_list.append({"module_name": module_name, "usage_count": usage_count})
+    try:
+        for module_doc in module_usage_collection.find():
+            module_name = module_doc["module_name"]
+            usage_count = module_doc["usage_count"]
+            result_list.append({"module_name": module_name, "usage_count": usage_count})
     return result_list
