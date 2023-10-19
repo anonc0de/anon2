1c6f47d30ca68da3168c374ae36eba6299c1c67e
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 00:02:21 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -1,15 +1,3 @@
-"""
-from importlib import import_module
-
-from PyroUbot.modules import loadModule
-
-modules = loadModule()
-for mod in modules:
-      imported_module = import_module(f"PyroUbot.modules.{mod}")
-      module_name = getattr(imported_module, "__MODULE__", "").replace(" ", "_").lower()
-print(mod)
-"""
-
 from importlib import import_module
 
 from PyroUbot.modules import loadModule
