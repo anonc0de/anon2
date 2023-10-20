00380e13e0ca594d573d94024d889a0b1d1cd34e
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 21:19:58 +0700

    Update cmd.py

--- PyroUbot/core/database/cmd.py
+++ PyroUbot/core/database/cmd.py
@@ -7,13 +7,13 @@ async def get_module_usage(module_name):
     module = await modules.find_one({"module_name": module_name})
     if not module:
         return 0
-    return module.get("usage_count", 0)
+    return module.get("usage_count")
 
 async def record_module_usage(module_name):
     try:
         module = await modules.find_one({"module_name": module_name})
         if module:
-            usage_count = modules.get("usage_count", 0)
+            usage_count = modules.get("usage_count")
             if module_name in usage_count:
                 usage_count[module_name] += 1
             else:
@@ -30,7 +30,7 @@ async def remove_module_usage(module_name):
     try:
         module = await modules.find_one({"module_name": module_name})
         if module:
-            usage_count = module.get("usage_count", 0)
+            usage_count = module.get("usage_count")
             if usage_count > 0:
                 await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count - 1}})
         return True
