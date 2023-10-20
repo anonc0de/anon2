6a291c56482bcb616c366043251796413235591f
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 01:15:56 +0700

    Update blacklist.py

--- PyroUbot/modules/blacklist.py
+++ PyroUbot/modules/blacklist.py
@@ -19,16 +19,19 @@ __HELP__ = f"""
 
 
 @PY.UBOT("addbl", FILTERS.ME_GROUP)
+@PY.TOP_CMD
 async def _(client, message):
     await add_blaclist(client, message)
 
 
 @PY.UBOT("unbl", FILTERS.ME_GROUP)
+@PY.TOP_CMD
 async def _(client, message):
     await del_blacklist(client, message)
 
 
 @PY.UBOT("rallbl")
+@PY.TOP_CMD
 async def _(client, message):
     await rem_all_blacklist(client, message)
 
