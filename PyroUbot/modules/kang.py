474526932bd724ed1f6c1430fdaa1a08831540ca
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 03:19:51 +0700

    Update kang.py

--- PyroUbot/modules/kang.py
+++ PyroUbot/modules/kang.py
@@ -4,7 +4,7 @@ __MODULE__ = "kang"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴋᴀɴɢ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}kang</code> [ʀᴇᴘʟʏ ᴛᴏ ɪᴍᴀɢᴇ/sᴛɪᴄᴋᴇʀ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>kang</code> [ʀᴇᴘʟʏ ᴛᴏ ɪᴍᴀɢᴇ/sᴛɪᴄᴋᴇʀ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴅᴀɴ ᴄᴏsᴛᴜᴍ ᴇᴍᴏᴊɪ sᴛɪᴄᴋᴇʀ ᴋᴇ sᴛɪᴄᴋᴇʀ ᴘᴀᴄᴋ
 
   <b>ɴᴏᴛᴇ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ᴘᴀᴋᴇᴛ ꜱᴛɪᴋᴇʀ ʙᴀʀᴜ ɢᴜɴᴀᴋᴀɴ ᴀɴɢᴋᴀ ᴅɪ ʙᴇʟᴀᴋᴀɴɢ !ᴋᴀɴɢ.
@@ -12,11 +12,8 @@ __HELP__ = f"""
 """
 
 
-#@PY.BOT("kang")
-#async def _(client, message):
-    #await kang_cmd_bot(client, message)
-
 
 @PY.UBOT("kang")
+@PY.TOP_CMD
 async def _(client, message):
     await kang(client, message)
