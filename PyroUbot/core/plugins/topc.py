de76d56ec343f5cd87cc0c1c046b8504e6237198
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 21:35:33 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -8,12 +8,30 @@ for module, count in module_counts.items():
 """
 
 async def get_top_module(client, message):
-    module_name = message.command[1].replace("_", " ")
+    imported_module = import_module(f"PyroUbot.modules")
+    module_name = getattr(imported_module, "__MODULE__", "").replace(" ", "_").lower()
+
     usage_count = await get_module_usage(module_name)
 
     if usage_count is not None:
-        response = f"Modul {module_name} telah digunakan sebanyak {usage_count} kali."
+        response += f"•> {module_name} : {usage_count} ."
     else:
-        response = f"Tidak ada hasil penggunaan modul {module_name} yang ditemukan."
+        response += f"•> {module_name} : 0."
 
     await message.reply(response)
+
+
+"""
+for user_id in await get_seles():
+        try:
+            user = await bot.get_users(user_id)
+            user = f"<a href=tg://user?id={user.id}>{user.first_name} {user.last_name or ''}</a> > <code>{user.id}</code>"
+        except Exception:
+            continue
+        text += f"   •> {user}\n"
+    if not text:
+        await message.reply_text("Tᴛɪᴅᴀᴋ ᴀᴅᴀ ᴘᴇɴɢɢᴜɴᴀ ʏᴀɴɢ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
+    else:
+        await message.reply_text(text)
+
+"""
