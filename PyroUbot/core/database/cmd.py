a0d63166366518025d92c79cf6ee60777856e053
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 21:22:32 +0700

    Update cmd.py

--- PyroUbot/core/database/cmd.py
+++ PyroUbot/core/database/cmd.py
@@ -7,20 +7,17 @@ async def get_module_usage(module_name):
     module = await modules.find_one({"module_name": module_name})
     if not module:
         return 0
-    return module.get("usage_count")
+    return module.get("usage_count", 0)
 
 async def record_module_usage(module_name):
     try:
         module = await modules.find_one({"module_name": module_name})
         if module:
-            usage_count = modules.get("usage_count")
-            if module_name in usage_count:
-                usage_count[module_name] += 1
-            else:
-                usage_count[module_name] = 1
+            usage_count = module.get("usage_count", 0)
+            usage_count += 1
             await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count}})
         else:
-            await modules.insert_one({"module_name": module_name, "usage_count": {module_name: 1}})
+            await modules.insert_one({"module_name": module_name, "usage_count": 1})
         return True
     except PyMongoError as e:
         print(f"Error in recording module usage: {str(e)}")
@@ -30,9 +27,10 @@ async def remove_module_usage(module_name):
     try:
         module = await modules.find_one({"module_name": module_name})
         if module:
-            usage_count = module.get("usage_count")
+            usage_count = module.get("usage_count", 0)
             if usage_count > 0:
-                await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count - 1}})
+                usage_count -= 1
+                await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count}})
         return True
     except PyMongoError as e:
         print(f"Error in removing module usage: {str(e)}")
