3a0603eee61ae976189cb2d925e4a28776ceac2c
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 03:19:11 +0700

    Update joinned.py

--- PyroUbot/modules/joinned.py
+++ PyroUbot/modules/joinned.py
@@ -4,39 +4,43 @@ __MODULE__ = "joinleave"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴊᴏɪɴʟᴇᴀᴠᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}kickme</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>kickme</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴋᴇʟᴜᴀʀ ᴅᴀʀɪ ɢʀᴜᴘ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}join</code> [ᴜꜱᴇʀɴᴀᴍᴇɢᴄ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>join</code> [ᴜꜱᴇʀɴᴀᴍᴇɢᴄ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴊᴏɪɴ ᴋᴇ ɢʀᴜᴘ ᴍᴇʟᴀʟᴜɪ ᴜꜱᴇʀɴᴀᴍᴇ 
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}leaveallgc</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>leaveallgc</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴋᴇʟᴜᴀʀ ᴅᴀʀɪ ꜱᴇᴍᴜᴀ ɢʀᴜᴘ 
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}leaveallch</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>leaveallch</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴋᴇʟᴜᴀʀ ᴅᴀʀɪ ꜱᴇᴍᴜᴀ ᴄʜᴀɴɴᴇʟ 
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}leave</code> [ᴜꜱᴇʀɴᴀᴍᴇɢᴄ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>leave</code> [ᴜꜱᴇʀɴᴀᴍᴇɢᴄ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴋᴇʟᴜᴀʀ ᴅᴀʀɪ ɢʀᴜᴘ ᴍᴇʟᴀʟᴜɪ ᴜꜱᴇʀɴᴀᴍᴇ
 """
 
 
-@PY.UBOT("kickme|leave", FILTERS.ME_GROUP)
+@PY.UBOT("kickme", FILTERS.ME_GROUP)
+@PY.TOP_CMD
 async def _(client, message):
     await leave(client, message)
 
 
 @PY.UBOT("join")
+@PY.TOP_CMD
 async def _(client, message):
     await join(client, message)
 
 
 @PY.UBOT("leaveallgc")
+@PY.TOP_CMD
 async def _(client, message):
     await kickmeall(client, message)
 
 
 @PY.UBOT("leaveallch")
+@PY.TOP_CMD
 async def _(client, message):
     await kickmeallch(client, message)
 
