dd3a4f6edd6c53e520dffd1aaff0404448f936a5
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 20:30:15 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -8,14 +8,13 @@ for module, count in module_counts.items():
 """
 
 async def get_top_module(client, message):
-    module_results = await get_module_usage(message.command[1])
-
+    module_name = message.command[1]
+    module_results = await get_module_usage(module_name)
     if not module_results:
         await message.reply("Tidak ada hasil penggunaan modul yang ditemukan.")
     else:
         response = "Hasil penggunaan modul:\n"
         for result in module_results:
-            module_name = getattr(imported_module, "__MODULE__", "").replace(" ", "_").lower()
             usage_count = result["usage_count"]
             response += f"Modul {module_name}: {usage_count} kali digunakan\n"
         await message.reply(response)
