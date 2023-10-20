84d8fe1ad990b33cb05140783dee2ce81874bcd0
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 01:35:09 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -1,46 +1,13 @@
-from importlib import import_module
-
 from PyroUbot import *
 
 
-async def get_top_module(client, message):
+async def get_top_modules(client, message):
     text = "<b>🗂️ᴅᴀғᴛᴀʀ ᴍᴏᴅᴜʟᴇ ᴜʙᴏᴛ\n</b>"
-    modules = loadModule()
-    for module_name in modules:
-        count = await get_module_usage(module_name)
-        text += f"•> {module_name} : {count} \n"
-    if not text:
+    me_id = client.me.id
+    modules = await all_vars(me_id, "modules")
+    if not modules:
         await message.reply_text("ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴍᴏᴅᴜʟᴇ ʏᴀɴɢ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
     else:
+        for module in modules:
+            text += f"•>{module} \n"
         await message.reply_text(text)
-
-"""
-async def get_top_module(client, message):
-    moduler = loadModule() 
-    imported_module = import_module(f"PyroUbot.modules.{moduler}")
-    module_name = getattr(imported_module, "__MODULE__", "").replace(" ", "_").lower()
-
-    usage_count = await get_module_usage(module_name)
-
-    if usage_count is not None:
-        response += f"•> {module_name} : {usage_count} ."
-    else:
-        response += f"•> {module_name} : 0."
-
-    await message.reply(response)
-
-
-
-for user_id in await get_seles():
-        try:
-            user = await bot.get_users(user_id)
-            user = f"<a href=tg://user?id={user.id}>{user.first_name} {user.last_name or ''}</a> > <code>{user.id}</code>"
-        except Exception:
-            continue
-        text += f"   •> {user}\n"
-    if not text:
-        await message.reply_text("Tᴛɪᴅᴀᴋ ᴀᴅᴀ ᴘᴇɴɢɢᴜɴᴀ ʏᴀɴɢ ᴅɪᴛᴇᴍᴜᴋᴀɴ")
-    else:
-        await message.reply_text(text)
-
-"""
