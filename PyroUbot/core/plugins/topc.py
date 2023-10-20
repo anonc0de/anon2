1625307d07e7247caff1135b31455ed9e7904a13
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 04:02:36 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -1,6 +1,6 @@
 from PyroUbot import *
 
-
+"""
 async def get_top_module(client, message):
     vars = await all_vars(bot.me.id, "modules")
     sorted_vars = sorted(vars.items(), key=lambda item: item[1], reverse=True)
@@ -13,3 +13,29 @@ async def get_top_module(client, message):
     txt += f"Total: {total_count} commands"
 
     await message.reply(txt)
+    """
+
+
+async def get_top_module(client, message):
+    vars = await all_vars(client.me.id, "modules")
+    sorted_vars = sorted(vars.items(), key=lambda item: item[1], reverse=True)
+
+    command_count = 5
+    text = message.text.split()
+
+    if len(text) == 2:
+        try:
+            command_count = min(max(int(text[1]), 1), 10)
+        except ValueError:
+            pass
+
+    total_count = sum(count for _, count in sorted_vars[:command_count])
+
+    txt = "📊ᴛᴏᴘ ᴄᴏᴍᴍᴀɴᴅ\n"
+    for command, count in sorted_vars[:command_count]:
+        txt += f" •> {command} : {count}\n"
+
+    txt += f"📈ᴛᴏᴛᴀʟ: {total_count} ᴄᴏᴍᴍᴀɴᴅ"
+
+    await message.reply(txt)
+
