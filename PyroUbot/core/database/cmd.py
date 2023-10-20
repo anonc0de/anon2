d95591481dbe8f596c4e70af0c129860907f4a25
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-20 21:08:09 +0700

    Update cmd.py

--- PyroUbot/core/database/cmd.py
+++ PyroUbot/core/database/cmd.py
@@ -14,9 +14,13 @@ async def record_module_usage(module_name):
         module = await modules.find_one({"module_name": module_name})
         if module:
             usage_count = module.get("usage_count", 0)
-            await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count + 1}})
+            if module_name in usage_count:
+                usage_count[module_name] += 1
+            else:
+                usage_count[module_name] = 1
+            await modules.update_one({"module_name": module_name}, {"$set": {"usage_count": usage_count}})
         else:
-            await modules.insert_one({"module_name": module_name, "usage_count": 1})
+            await modules.insert_one({"module_name": module_name, "usage_count": {module_name: 1}})
         return True
     except PyMongoError as e:
         print(f"Error in recording module usage: {str(e)}")
