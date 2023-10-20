f2f7e64525e73f71574fcf5e28548e37a7035d94
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-21 01:39:43 +0700

    Update topc.py

--- PyroUbot/core/plugins/topc.py
+++ PyroUbot/core/plugins/topc.py
@@ -1,7 +1,7 @@
 from PyroUbot import *
 
 
-async def get_top_modules(client, message):
+async def get_top_module(client, message):
     vars = await all_vars(bot.me.id, "modules")
     txt = "ketik text\n"
     for command, count in vars.items():
