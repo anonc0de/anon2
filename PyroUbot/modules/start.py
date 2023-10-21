bd140f3ce7a5660f873c35a196bc1186c00fea35
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-22 02:32:56 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -11,3 +11,13 @@ async def _(client, message):
 @PY.PRIVATE
 async def _(client, message):
     await start_cmd(client, message)
+
+
+@PY.BOT("flip")
+async def _(client, message):
+    await flip_coin_command(client, message)
+
+
+@PY.BOT("saldo")
+async def _(client, message):
+    await balance_command(client, message)
