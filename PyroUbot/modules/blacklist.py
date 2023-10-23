ce6f19017d53270182aae804f9e53cf3c3c20c1f
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 01:54:57 +0700

    Update blacklist.py

--- PyroUbot/modules/blacklist.py
+++ PyroUbot/modules/blacklist.py
@@ -4,38 +4,43 @@ __MODULE__ = "blacklist"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ʙʟᴀᴄᴋʟɪsᴛ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}addbl</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>addbl</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍᴀsᴜᴋᴋᴀɴ ɢʀᴏᴜᴘ ᴋᴇ ᴅᴀꜰᴛᴀʀ ʜɪᴛᴀᴍ sᴜᴘᴀʏᴀ ɢᴄᴀsᴛ ᴋᴀʟɪᴀɴ ᴛɪᴅᴀᴋ ᴍᴀsᴜᴋ ᴋᴇ ɢʀᴏᴜᴘ [ʟᴀᴋᴜᴋᴀɴ ᴅɪ ɢʀᴏᴜᴘ, sᴇʟᴀɪɴ ᴅɪ ɢʀᴏᴜᴘ ʙᴏᴛ ᴛɪᴅᴀᴋ ᴀᴋᴀɴ ʀᴇsᴘᴏɴ]
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}unbl</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>unbl</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs ɢʀᴏᴜᴘ ᴅᴀʀɪ ᴅᴀꜰᴛᴀʀ ʜɪᴛᴀᴍ ᴀɢᴀʀ ɢᴄᴀsᴛ ʙɪsᴀ ᴍᴀsᴜᴋ ᴋᴇ ɢʀᴏᴜᴘ  [ʟᴀᴋᴜᴋᴀɴ ᴅɪ ɢʀᴏᴜᴘ, sᴇʟᴀɪɴ ᴅɪ ɢʀᴏᴜᴘ ʙᴏᴛ ᴛɪᴅᴀᴋ ᴀᴋᴀɴ ʀᴇsᴘᴏɴ]
   
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}rallbl</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>rallbl</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs sᴇᴍᴜᴀ ʙʟᴀᴄᴋʟɪsᴛ
   
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}listbl</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>listbl</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍᴇʀɪᴋsᴀ ᴅᴀꜰᴛᴀʀ ʙʟᴀᴄᴋʟɪsᴛ ɢʀᴏᴜᴘ
 """
 
 
 @PY.UBOT("addbl", FILTERS.ME_GROUP)
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await add_blaclist(client, message)
 
 
 @PY.UBOT("unbl", FILTERS.ME_GROUP)
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await del_blacklist(client, message)
 
 
 @PY.UBOT("rallbl")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await rem_all_blacklist(client, message)
 
 
 @PY.UBOT("listbl")
+@PY.SUDO()
+@PY.TOP_CMD
 async def _(client, message):
     await get_blacklist(client, message)
