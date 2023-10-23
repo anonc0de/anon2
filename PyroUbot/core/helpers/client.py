06406008a4f362735a77049d1fd11db05207586b
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 03:54:53 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -6,8 +6,6 @@ from PyroUbot import *
 
 async def check_sudo(client, user_id):
     sudo_id = await get_list_from_vars(client.me.id, "SUDO_USERS")
-    if client.me.id not in sudo_id:
-        sudo_id.append(client.me.id)
     return user_id in sudo_id
 
 
