f7ae05da6339e2daac06039d8d697012e0999dce
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 02:05:32 +0700

    Update image.py

--- PyroUbot/modules/image.py
+++ PyroUbot/modules/image.py
@@ -4,39 +4,43 @@ __MODULE__ = "image"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɪᴍᴀɢᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}rbg</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>rbg</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs ʟᴀᴛᴀʀ ʙᴇʟᴀᴋᴀɴɢ ɢᴀᴍʙᴀʀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}blur</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>blur</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀ ᴇꜰᴇᴋ ʙʟᴜʀ ᴋᴇ ɢᴀᴍʙᴀʀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}miror</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>miror</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀɴ ᴇꜰᴇᴋ ᴄᴇʀᴍɪɴ ᴋᴇ ɢᴀᴍʙᴀʀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}negative</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>negative</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀɴ ᴇꜰᴇᴋ ɴᴇɢᴀᴛɪᴠᴇ ᴋᴇ ɢᴀᴍʙᴀʀ
 """
 
 
 @PY.UBOT("rbg")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await rbg_cmd(client, message)
 
 
 @PY.UBOT("blur")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await blur_cmd(client, message)
 
 
 @PY.UBOT("negative")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await negative_cmd(client, message)
 
 
 @PY.UBOT("miror")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await miror_cmd(client, message)
