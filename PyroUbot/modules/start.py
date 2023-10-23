6a7241831972e42753f5662708efbd2dd6825f0b
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-10-23 23:46:28 +0700

    Update start.py

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -2,9 +2,9 @@ from .. import *
 
 
 # @PY.UBOT("ping")
-@ubot.on_message(filters.command("ping"))
+@ubot.on_message(ubot.cmd_prefix("ping"))
 @PY.TOP_CMD
-# @PY.SUDO
+@PY.SUDO
 async def _(client, message):
     await ping_cmd(client, message)
 
