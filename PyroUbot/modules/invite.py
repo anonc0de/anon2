0eb1dc6f723b22dbe0f6cea05150068dc361328c
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 02:06:44 +0700

    Update invite.py

--- PyroUbot/modules/invite.py
+++ PyroUbot/modules/invite.py
@@ -4,30 +4,33 @@ __MODULE__ = "invite"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɪɴᴠɪᴛᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}invite</code> [ᴜsᴇʀɴᴀᴍᴇ] 
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>invite</code> [ᴜsᴇʀɴᴀᴍᴇ] 
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴜɴᴅᴀɴɢ ᴀɴɢɢᴏᴛᴀ ᴋᴇ ɢʀᴜᴘ ᴀɴᴅᴀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}inviteall</code> [ᴜsᴇʀɴᴀᴍᴇ_ɢʀᴏᴜᴘ - ᴄᴏʟʟᴅᴏᴡɴ=ᴅᴇᴛɪᴋ ᴘᴇʀ ɪɴᴠɪᴛᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>inviteall</code> [ᴜsᴇʀɴᴀᴍᴇ_ɢʀᴏᴜᴘ - ᴄᴏʟʟᴅᴏᴡɴ=ᴅᴇᴛɪᴋ ᴘᴇʀ ɪɴᴠɪᴛᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴜɴᴅᴀɴɢ ᴀɴɢɢᴏᴛᴀ ᴅᴀʀɪ ᴏʙʀᴏʟᴀɴ ɢʀᴜᴘ ʟᴀɪɴ ᴋᴇ ᴏʙʀᴏʟᴀɴ ɢʀᴜᴘ ᴀɴᴅᴀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}cancel</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>cancel</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴀᴛᴀʟᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴᴠɪᴛᴇᴀʟʟ
   """
 
 
 @PY.UBOT("invite")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await invite_cmd(client, message)
 
 
 @PY.UBOT("inviteall")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await inviteall_cmd(client, message)
 
 
 @PY.UBOT("cancel")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await cancel_cmd(client, message)
