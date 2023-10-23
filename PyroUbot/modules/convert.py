e1164c3423e16d45beea15661382c14f4df48834
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 03:03:47 +0700

    Update convert.py

--- PyroUbot/modules/convert.py
+++ PyroUbot/modules/convert.py
@@ -25,42 +25,36 @@ __HELP__ = f"""
 
 
 @PY.UBOT("toanime")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await convert_anime(client, message)
 
 
 @PY.UBOT("toimg")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await convert_photo(client, message)
 
 
 @PY.UBOT("tosticker")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await convert_sticker(client, message)
 
 
 @PY.UBOT("togif")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await convert_gif(client, message)
 
 
 @PY.UBOT("toaudio")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await convert_audio(client, message)
 
 
 @PY.UBOT("colong")
-@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await colong_cmn(client, message)
