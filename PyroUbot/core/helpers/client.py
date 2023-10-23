df60ea42025cbcf3c7ff45813b6962bd67ffc0fa
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 03:16:07 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -4,11 +4,11 @@ from pyrogram.enums import ChatType
 from PyroUbot import *
 
 
-async def check_sudo(client, user_id):
+async def check_sudo(client, message):
     sudo_id = await get_list_from_vars(client.me.id, "SUDO_USERS")
     if client.me.id not in sudo_id:
         sudo_id.append(client.me.id)
-    return user_id in sudo_id
+    return message.from_user.id in sudo_id
 
 
 
