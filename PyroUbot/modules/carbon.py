6a361ef76ae5a0421c401e036e14bb499006a5aa
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 01:55:19 +0700

    Update carbon.py

--- PyroUbot/modules/carbon.py
+++ PyroUbot/modules/carbon.py
@@ -4,12 +4,13 @@ __MODULE__ = "carbon"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ꜰᴏɴᴛ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}carbon</code> [ʀᴇᴘʟʏ/ᴛᴇxᴛ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>carbon</code> [ʀᴇᴘʟʏ/ᴛᴇxᴛ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ᴛᴇᴋꜱ ᴄᴀʀʙᴏɴᴀʀᴀ
 """
 
 
 @PY.UBOT("carbon")
+@PY.SUDO()
 @PY.TOP_CMD
 async def _(client, message):
     await carbon_func(client, message)
