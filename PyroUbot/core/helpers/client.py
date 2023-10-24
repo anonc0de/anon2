51e1f9b6ca5c565e1e296e5dbb48194e94c61bb7
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-24 18:52:00 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -5,11 +5,10 @@ from PyroUbot import *
 
 
 async def check_sudo(client, user_id):
-    me = await client.get_me()
-    sudo_users = await client.get_list_from_vars(me.id, "SUDO_USERS")
-    if me.id not in sudo_users:
-        sudo_users.append(me.id)
-    return user_id in sudo_users
+    sudo_id = await get_list_from_vars(client.me.id, "SUDO_USERS")
+    if client.me.id not in sudo_id:
+        sudo_id.append(client.me.id)
+    return user_id in sudo_id
 
 
 class FILTERS:
@@ -133,11 +132,12 @@ class PY:
     def SUDO(command=None):
         def decorator(func):
             async def wrapper(client, message):
-                if not command or message.get_command() == command:
+                if command is None or message.text == command:
                     if await check_sudo(client, message.from_user.id):
-                        await func(client, message)
+                        return await func(client, message)
 
             return wrapper
+
         return decorator
 
 """
