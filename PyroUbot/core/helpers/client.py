8a3ae9e6f0ac85401d021a11c152783e80054e93
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-25 23:28:21 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -151,17 +151,4 @@ class PY:
 
         return function
         
-    @staticmethod
-    def SUDO(command):
-        def wrapper(func):
-            @ubot.on_message(ubot.cmd_prefix(command))
-            async def wrapped_func(client, message):
-                sudo_id = await get_list_from_vars(client.me.id, "SUDO_USERS")
-                user_id = message.from_user.id if message.from_user else message.chat.id
-                if user_id in sudo_id:
-                    await func(client, message)
-
-            return wrapped_func
-
-        return wrapper
-
+    
