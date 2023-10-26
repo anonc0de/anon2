82e6594bc40f6199575e90075b420bb0f4e93300
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:47:00 +0700

    Update profiles.py

--- PyroUbot/modules/profiles.py
+++ PyroUbot/modules/profiles.py
@@ -4,39 +4,39 @@ __MODULE__ = "profiles"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴘʀᴏꜰɪʟᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}setbio</code> [ᴛᴇxᴛ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>setbio</code> [ᴛᴇxᴛ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴜʙᴀʜ ʙɪᴏ ᴀɴᴅᴀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}setname</ᴄᴏᴅᴇ> [ᴛᴇxᴛ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>setname</ᴄᴏᴅᴇ> [ᴛᴇxᴛ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴜʙᴀʜ ɴᴀᴍᴀ ᴀɴᴅᴀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}block</code> [ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>block</code> [ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙʟᴏᴋɪʀ ᴘᴇɴɢɢᴜɴᴀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}unblock</code> [ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>unblock</code> [ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴋᴀ ʙʟᴏᴋɪʀ ᴘᴇɴɢɢᴜɴᴀ
 """
 
 
-@PY.UBOT("setbio")
+@PY.UBOT("setbio", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await set_bio(client, message)
 
 
-@PY.UBOT("setname")
+@PY.UBOT("setname", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await setname(client, message)
 
 
-@PY.UBOT("block")
+@PY.UBOT("block", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await block_user_func(client, message)
 
 
-@PY.UBOT("unblock")
+@PY.UBOT("unblock", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await unblock_user_func(client, message)
