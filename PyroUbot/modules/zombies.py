1abf390ef9fbaac83eb2408607d771c9c35776ae
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:54:29 +0700

    Update zombies.py

--- PyroUbot/modules/zombies.py
+++ PyroUbot/modules/zombies.py
@@ -4,12 +4,12 @@ __MODULE__ = "zombies"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴢᴏᴍʙɪᴇs 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}zombies</code>
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>zombies</code>
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇʟᴜᴀʀᴋᴀɴ ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs ᴅɪɢʀᴜᴘ ᴀɴᴅᴀ.
 """
 
 
-@PY.UBOT("zombies")
+@PY.UBOT("zombies", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await zombies_cmd(client, message)
