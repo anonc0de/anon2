7a6541e801230c4ab6386eb4e173a542e3e6ddc1
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:48:39 +0700

    Update quotly.py

--- PyroUbot/modules/quotly.py
+++ PyroUbot/modules/quotly.py
@@ -4,12 +4,12 @@ __MODULE__ = "quotly"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ǫᴜᴏᴛʟʏ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}q</code> [ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>q</code> [ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ ᴛᴇxᴛ ᴍᴇɴᴊᴀᴅɪ sᴛɪᴄᴋᴇʀ
 """
 
 
-@PY.UBOT("q")
+@PY.UBOT("q", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await quotly_cmd(client, message)
