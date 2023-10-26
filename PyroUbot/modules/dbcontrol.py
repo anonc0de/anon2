6abfb0a2845bbce5da73ed937268905a984f2fb4
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:08:02 +0700

    Update dbcontrol.py

--- PyroUbot/modules/dbcontrol.py
+++ PyroUbot/modules/dbcontrol.py
@@ -6,47 +6,56 @@ async def _(client, message):
     await prem_user(client, message)
 
 
-@PY.BOT("unprem", FILTERS.OWNER)
+@PY.BOT("unprem")
+@PY.OWNER
 async def _(client, message):
     await unprem_user(client, message)
 
 
-@PY.BOT("getprem", FILTERS.OWNER)
+@PY.BOT("getprem")
+@PY.OWNER
 async def _(cliebt, message):
     await get_prem_user(client, message)
 
 
-@PY.BOT("seles", FILTERS.OWNER)
+@PY.BOT("seles")
+@PY.OWNER
 async def _(client, message):
     await seles_user(client, message)
 
 
-@PY.BOT("unseles", FILTERS.OWNER)
+@PY.BOT("unseles")
+@PY.OWNER
 async def _(client, message):
     await unseles_user(client, message)
 
 
-@PY.BOT("getseles", FILTERS.OWNER)
+@PY.BOT("getseles")
+@PY.OWNER
 async def _(client, message):
     await get_seles_user(client, message)
 
 
-@PY.BOT("top", FILTERS.OWNER)
+@PY.BOT("top")
+@PY.OWNER
 async def _(client, message):
     await get_top_module(client, message)
 
 
-@PY.BOT("time", FILTERS.OWNER)
+@PY.BOT("time")
+@PY.OWNER
 async def _(client, message):
     await expired_add(client, message)
 
 
-@PY.BOT("cek", FILTERS.OWNER)
+@PY.BOT("cek")
+@PY.OWNER
 async def _(client, message):
     await expired_cek(client, message)
 
 
-@PY.BOT("untime", FILTERS.OWNER)
+@PY.BOT("untime")
+@PY.OWNER
 async def _(client, message):
     await un_expired(client, message)
 
@@ -55,7 +64,3 @@ async def _(client, message):
 async def _(client, callback_query):
     await cb_restart(client, callback_query)
 
-
-@PY.CALLBACK("gitpull")
-async def _(client, callback_query):
-    await cb_gitpull(client, callback_query)
