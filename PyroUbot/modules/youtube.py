5a5d63be7b46e62bf839ecc95c9d0553bb1665c9
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:53:58 +0700

    Update youtube.py

--- PyroUbot/modules/youtube.py
+++ PyroUbot/modules/youtube.py
@@ -4,21 +4,21 @@ __MODULE__ = "youtube"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ʏᴏᴜᴛᴜʙᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}song</code> [sᴏɴɢ ᴛɪᴛʟᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>song</code> [sᴏɴɢ ᴛɪᴛʟᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b>  ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴏᴡɴʟᴏᴀᴅ ᴍᴜsɪᴄ ʏᴀɴɢ ᴅɪɪɴɢɪɴᴋᴀɴ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}vsong</code> [sᴏɴɢ ᴛɪᴛʟᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>vsong</code> [sᴏɴɢ ᴛɪᴛʟᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴏᴡɴʟᴏᴀᴅ ᴠɪᴅᴇᴏ ʏᴀɴɢ ᴅɪɪɴɢɪɴᴋᴀɴ
 """
 
 
-@PY.UBOT("vsong")
+@PY.UBOT("vsong", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await vsong_cmd(client, message)
 
 
-@PY.UBOT("song")
+@PY.UBOT("song"), SUDO=True
 @PY.TOP_CMD
 async def _(client, message):
     await song_cmd(client, message)
