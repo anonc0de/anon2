4e6452b7b9a69a693491b83762753b606f2b81d8
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:49:09 +0700

    Update sangmata.py

--- PyroUbot/modules/sangmata.py
+++ PyroUbot/modules/sangmata.py
@@ -4,12 +4,12 @@ __MODULE__ = "sangmata"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴀɴɢᴍᴀᴛᴀ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}sg</code> [ᴜsᴇʀ_ɪᴅ/ʀᴇᴘʟʏ ᴜsᴇʀ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>sg</code> [ᴜsᴇʀ_ɪᴅ/ʀᴇᴘʟʏ ᴜsᴇʀ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍᴇʀɪᴋsᴀ ʜɪsᴛᴏʀɪ ɴᴀᴍᴀ/ᴜsᴇʀɴᴀᴍᴇ
 """
 
 
-@PY.UBOT("sg")
+@PY.UBOT("sg", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await sg_cmd(client, message)
