40b09e6b645066d8e6ace113ab9a04395d7713aa
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:47:51 +0700

    Update purge.py

--- PyroUbot/modules/purge.py
+++ PyroUbot/modules/purge.py
@@ -4,30 +4,30 @@ __MODULE__ = "purge"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴘᴜʀɢᴇ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}purge</code> [ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>purge</code> [ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ʙᴇʀsɪʜᴋᴀɴ (ʜᴀᴘᴜs sᴇᴍᴜᴀ ᴘᴇsᴀɴ) ᴏʙʀᴏʟᴀɴ ᴅᴀʀɪ ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪʙᴀʟᴀs ʜɪɴɢɢᴀ ʏᴀɴɢ ᴛᴇʀᴀᴋʜɪʀ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}del</code> [ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>del</code> [ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ʜᴀᴘᴜs ᴘᴇsᴀɴ ʏᴀɴɢ ᴅɪʙᴀʟᴀs
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}purgeme</code> [ɴᴜᴍʙᴇʀ ᴏꜰ ᴍᴇssᴀɢᴇs]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>purgeme</code> [ɴᴜᴍʙᴇʀ ᴏꜰ ᴍᴇssᴀɢᴇs]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ʜᴀᴘᴜs ᴘᴇsᴀɴ ᴀɴᴅᴀ sᴇɴᴅɪʀɪ ᴅᴇɴɢᴀɴ ᴍᴇɴᴇɴᴛᴜᴋᴀɴ ᴛᴏᴛᴀʟ ᴘᴇsᴀɴ
 """
 
 
-@PY.UBOT("del")
+@PY.UBOT("del", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await del_cmd(client, message)
 
 
-@PY.UBOT("purgeme")
+@PY.UBOT("purgeme", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await purgeme_cmd(client, message)
 
 
-@PY.UBOT("purge")
+@PY.UBOT("purge", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await purge_cmd(client, message)
