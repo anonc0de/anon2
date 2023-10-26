ea3395650eff6681230062bab6d82fa0098ec7bb
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:51:28 +0700

    Update telegraph.py

--- PyroUbot/modules/telegraph.py
+++ PyroUbot/modules/telegraph.py
@@ -4,12 +4,12 @@ __MODULE__ = "telegraph"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴛᴇʟᴇɢʀᴀᴘʜ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}tg</code> [ʀᴇᴘʟʏ ᴍᴇᴅɪᴀ/ᴛᴇxᴛ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>tg</code> [ʀᴇᴘʟʏ ᴍᴇᴅɪᴀ/ᴛᴇxᴛ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴘʟᴏᴀᴅ ᴍᴇᴅɪᴀ/ᴛᴇxᴛ ᴋᴇ telegra.ph
 """
 
 
-@PY.UBOT("tg")
+@PY.UBOT("tg", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await tg_cmd(client, message)
