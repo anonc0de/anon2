5dd67d6789d5bb7c1bd5cb0e34867ac8bbd4ccb5
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-26 19:45:38 +0700

    Update openai.py

--- PyroUbot/modules/openai.py
+++ PyroUbot/modules/openai.py
@@ -4,30 +4,30 @@ __MODULE__ = "openai"
 __HELP__ = f"""
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴏᴘᴇɴᴀɪ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}ai</code> ᴏʀ <code>{PREFIX[0]}ask</code>  [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>ai</code> ᴏʀ <code>ask</code>  [ǫᴜᴇʀʏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴊᴜᴋᴀɴ ᴘᴇʀᴛᴀɴʏᴀᴀɴ ᴋᴇ ᴄʜᴀᴛɢᴘᴛ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}dalle</code> ᴏʀ <code>{PREFIX[0]}photo</code> [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>dalle</code> ᴏʀ <code>photo</code> [ǫᴜᴇʀʏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ sᴇʙᴜᴀʜ ᴘʜᴏᴛᴏ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}stt</code> [ʀᴇᴘʟʏ ᴠᴏɪᴄᴇ ɴᴏᴛᴇ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>stt</code> [ʀᴇᴘʟʏ ᴠᴏɪᴄᴇ ɴᴏᴛᴇ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ ᴘᴇsᴀɴ sᴜᴀʀᴀ ᴋᴇ ᴛᴇxᴛ
 """
 
 
-@PY.UBOT("ai|ask")
+@PY.UBOT("ai|ask", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await ai_cmd(client, message)
 
 
-@PY.UBOT("photo")
+@PY.UBOT("photo", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await dalle_cmd(client, message)
 
 
-@PY.UBOT("stt")
+@PY.UBOT("stt", SUDO=True)
 @PY.TOP_CMD
 async def _(client, message):
     await stt_cmd(client, message)
