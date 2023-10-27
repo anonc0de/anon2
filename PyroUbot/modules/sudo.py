9ba5b5b52e78a29cbc8f9505f860273facc9affa
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-10-28 02:16:56 +0700

    Update sudo.py

--- PyroUbot/modules/sudo.py
+++ PyroUbot/modules/sudo.py
@@ -10,87 +10,90 @@ from PyroUbot import *
 @PY.UBOT("addsudo")
 @PY.TOP_CMD
 async def _(client, message):
+    msg = await message.reply("sᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏsᴇs...")
     user_id = await extract_user(message)
     if not user_id:
-        return await message.reply(
-            "<b>➡️ Hᴀʀᴀᴘ ʙᴀʟᴀs ᴋᴇ ᴜsᴇʀ ᴀᴛᴀᴜ ᴋᴇᴛɪᴋ ᴜsᴇʀ_ɪᴅ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+        return await msg.edit(
+            "➡️ Hᴀʀᴀᴘ ʙᴀʟᴀs ᴋᴇ ᴜsᴇʀ ᴀᴛᴀᴜ ᴋᴇᴛɪᴋ ᴜsᴇʀ_ɪᴅ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
 
     try:
         user = await client.get_users(user_id)
     except Exception as error:
-        return await message.reply(error)
+        return await msg.edit(error)
 
     sudo_users = await get_list_from_vars(client.me.id, "SUDO_USERS", "DB_SUDO")
 
     if user.id in sudo_users:
-        return await message.reply(
-            f"<b>✨ [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) sᴜᴅᴀʜ ʙᴇʀᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+        return await msg.edit(
+            f"✨ {user.first_name} {user.last_name or ''} sᴜᴅᴀʜ ʙᴇʀᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
 
     try:
         await add_to_vars(client.me.id, "SUDO_USERS", user.id, "DB_SUDO")
-        return await message.reply(
-            f"<b>✅ [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) ʙᴇʀʜᴀsɪʟ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+        return await msg.edit(
+            f"✅ {user.first_name} {user.last_name or ''} ʙᴇʀʜᴀsɪʟ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
     except Exception as error:
-        return await message.reply(error)
+        return await msg.edit(error)
 
 
 @PY.UBOT("delsudo")
 @PY.TOP_CMD
 async def _(client, message):
+    msg = await message.reply("sᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏsᴇs...")
     user_id = await extract_user(message)
     if not user_id:
         return await message.reply(
-            "<b>➡️ Hᴀʀᴀᴘ ʙᴀʟᴀs ᴋᴇ ᴜsᴇʀ ᴀᴛᴀᴜ ᴋᴇᴛɪᴋ ᴜsᴇʀ_ɪᴅ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+            "➡️ Hᴀʀᴀᴘ ʙᴀʟᴀs ᴋᴇ ᴜsᴇʀ ᴀᴛᴀᴜ ᴋᴇᴛɪᴋ ᴜsᴇʀ_ɪᴅ ʏᴀɴɢ ɪɴɢɪɴ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ ᴋᴇ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
 
     try:
         user = await client.get_users(user_id)
     except Exception as error:
-        return await message.reply(error)
+        return await msg.edit(error)
 
     sudo_users = await get_list_from_vars(client.me.id, "SUDO_USERS", "DB_SUDO")
 
     if user.id not in sudo_users:
-        return await message.reply(
-            f"<b>✨ [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+        return await msg.edit(
+            f"✨ {user.first_name} {user.last_name or ''} ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴅᴀʟᴀᴍ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
 
     try:
         await remove_from_vars(client.me.id, "SUDO_USERS", user.id, "DB_SUDO")
-        return await message.reply(
-            f"<b>❌ [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀғᴛᴀʀ sᴜᴅᴏ</b>"
+        return await msg.edit(
+            f"❌ {user.first_name} {user.last_name or ''} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs ᴅᴀʀɪ ᴅᴀғᴛᴀʀ sᴜᴅᴏ"
         )
     except Exception as error:
-        return await message.reply(error)
+        return await msg.edit(error)
 
 
 @PY.UBOT("getsudo")
 @PY.TOP_CMD
 async def _(client, message):
+    msg = await message.reply("sᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏsᴇs...")
     sudo_users = await get_list_from_vars(client.me.id, "SUDO_USERS", "DB_SUDO")
 
     if not sudo_users:
-        return await message.reply("<s>ᴅᴀғᴛᴀʀ sᴜᴅᴏ ᴋᴏsᴏɴɢ</s>")
+        return await message.reply("ᴅᴀғᴛᴀʀ sᴜᴅᴏ ᴋᴏsᴏɴɢ")
 
     sudo_list = []
     for user_id in sudo_users:
         try:
             user = await client.get_users(int(user_id))
             sudo_list.append(
-                f" ├ [{user.first_name} {user.last_name or ''}](tg://user?id={user.id}) | <code>{user.id}</code>"
+                f" ├ {user.first_name} {user.last_name or ''} | {user.id}"
             )
         except:
-            pass
+            continue
 
     if sudo_list:
         response = (
-            "<b>❏ ᴅᴀғᴛᴀʀ sᴜᴅᴏ:</b>\n"
+            "❏ ᴅᴀғᴛᴀʀ sᴜᴅᴏ:\n"
             + "\n".join(sudo_list)
-            + f"\n<b>╰ ᴛᴏᴛᴀʟ sᴜᴅᴏ_ᴜsᴇʀs:</b> <code>{len(sudo_list)}</code>"
+            + f"\n ╰ ᴛᴏᴛᴀʟ sᴜᴅᴏ_ᴜsᴇʀs: {len(sudo_list)}"
         )
-        return await message.reply(response)
+        return await msg.edit(response)
     else:
-        return await message.reply("<b>ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇɴɢᴀᴍʙɪʟ ᴅᴀғᴛᴀʀ sᴜᴅᴏ sᴀᴀᴛ ɪɴɪ</b>")
+        return await msg.edit("ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇɴɢᴀᴍʙɪʟ ᴅᴀғᴛᴀʀ sᴜᴅᴏ sᴀᴀᴛ ɪɴɪ")
