90b0bdfdc8c125355c0860d1ff114c7f18e548a2
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2023-11-01 18:18:30 +0700

    Update add_ubot.py

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -16,21 +16,6 @@ async def _(client, callback_query):
     await ohaja(client, callback_query)
 
 
-@PY.CALLBACK("bahan")
-async def _(client, callback_query):
-    await need_api(client, callback_query)
-
-
-@PY.CALLBACK("memek")
-async def _(client, callback_query):
-    await bikin_memek(client, callback_query)
-
-
-@PY.CALLBACK("bahan")
-async def _(client, callback_query):
-    await need_api(client, callback_query)
-
-
 @PY.CALLBACK("memek")
 async def _(client, callback_query):
     await bikin_memek(client, callback_query)
