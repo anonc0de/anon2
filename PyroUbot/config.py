443ab66bac5f77ac167effb328986cc5b8610e2f
anonc0de <148568583+anonc0de@users.noreply.github.com>
2023-11-03 19:49:16 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,24 +1,20 @@
 import os
 
 DEVS = [
-    6629259024,
-    6862912822,
-    2066017531,
+    id
 ]
 
 API_ID = int(os.getenv("API_ID", "24623085"))
 
 API_HASH = os.getenv("API_HASH", "75ce0c6125ae201c9e3d5a825c667a91")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6590270131:AAEMI3qFnkYYXJWESCwm_VB395QsR8dLkGc")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "bot token")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "6629259024"))
-
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001462256506"))
+OWNER_ID = int(os.getenv("OWNER_ID", "id"))
 
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001538826310 -1001462256506 -1001812143750").split()))
 
-MAX_BOT = int(os.getenv("MAX_BOT", "550"))
+MAX_BOT = int(os.getenv("MAX_BOT", "10"))
 
 RMBG_API = os.getenv("RMBG_API", "a6qxsmMJ3CsNo7HyxuKGsP1o")
 
@@ -32,11 +28,6 @@ OPENAI_KEY = os.getenv(
 
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://ohmyshin12:ohmyshin12@cluster0.jboolzn.mongodb.net/?retryWrites=true&w=majority",
+    "mongoddb",
 )
 
-#mongodb+srv://dinexa1064:0212@cluster0.ep5evwf.mongodb.net/?retryWrites=true&w=majority
-
-#mongodb+srv://ilxnvuj833:0212@cluster0.ile0pdo.mongodb.net/?retryWrites=true&w=majority
-
-#mongodb+srv://odos1:odos1@odos.rdrfcsf.mongodb.net/?retryWrites=true&w=majority
