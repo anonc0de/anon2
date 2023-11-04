68b669fa30d5a6e0dba68326cf72c7e47d8da561
anonc0de <148568583+anonc0de@users.noreply.github.com>
2023-11-04 10:35:22 +0700

    Update alv.py

--- PyroUbot/core/plugins/alv.py
+++ PyroUbot/core/plugins/alv.py
@@ -28,7 +28,6 @@ async def alive_query(client, inline_query):
                 users = random.randrange(await my.get_dialogs_count())
                 group = random.randrange(await my.get_dialogs_count())
             get_exp = await get_expired_date(my.me.id)
-            exp = get_exp.strftime("%d-%m-%Y")
             if my.me.id == OWNER_ID:
                 status = "<b>ᴘʀᴇᴍɪᴜᴍ</b> <code>[˹ᴀ̷ɴσɴ˼]</code>"
             elif my.me.id in await get_seles():
